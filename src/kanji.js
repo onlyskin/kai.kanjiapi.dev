055afb00a53145e5557d68a0b7deb696047f4a9e
Sam Szreter <s.szreter@gmail.com>
2019-05-16 19:34:36 +0300

    Add ability to prioritise kanji words

---
+++ src/kanji.js
@@ -0,0 +1,108 @@
+function grade({grade}) {
+    return grade ? grade : '';
+}
+
+function jlpt({jlpt}) {
+    return jlpt ? jlpt : '';
+}
+
+function unicode({unicode}) {
+    return unicode ? `U+${unicode.toUpperCase()}` : '';
+}
+ 
+function validVariant(kanji, variant) {
+    return variant.written.includes(kanji);
+}
+
+function priorityVariant(variant) {
+    return variant.priorities.length > 0;
+}
+
+function scoreWord(word, kanji) {
+    const variantsWithKanji = word.variants
+        .filter(validVariant.bind(null, kanji));
+
+    const validPriorityVariant = variantsWithKanji
+        .find(priorityVariant);
+
+    const validPriorityVariantLength = (
+        validPriorityVariant ?
+        validPriorityVariant.priorities.length :
+        0
+    );
+
+    const hasPriorityVariant = word.variants
+        .some(priorityVariant);
+
+    const firstVariantIsValid = validVariant(kanji, word.variants[0]);
+
+    const minWordLength = (
+        validPriorityVariant ?
+        validPriorityVariant.written.length :
+        variantsWithKanji
+            .reduce((acc, curr) => Math.min(acc, curr.written.length), Infinity)
+    );
+
+    return {
+        validPriorityVariantLength,
+        firstVariantIsValid,
+        minWordLength,
+        hasPriorityVariant,
+    };
+}
+
+function compareWords(word1, word2, kanji) {
+    const score1 = scoreWord(word1, kanji);
+    const score2 = scoreWord(word2, kanji);
+
+    if (score1.validPriorityVariantLength !== score2.validPriorityVariantLength) {
+        return score2.validPriorityVariantLength - score1.validPriorityVariantLength;
+    }
+
+    if (score1.firstVariantIsValid !== score2.firstVariantIsValid) {
+        return score2.firstVariantIsValid - score1.firstVariantIsValid;
+    }
+
+    if (score1.hasPriorityVariant !== score2.hasPriorityVariant) {
+        return score2.hasPriorityVariant - score1.hasPriorityVariant;
+    }
+
+    return score1.minWordLength - score2.minWordLength;
+}
+
+function prioritiseWords(kanji, words) {
+    const wordsCopy = JSON.parse(JSON.stringify(words));
+    wordsCopy.sort((a, b) => compareWords(a, b, kanji))
+
+    return wordsCopy;
+}
+
+function wordsForKanji(kanji, words) {
+    const prioritised = prioritiseWords(kanji, words);
+    const converted = prioritised
+        .map(word => {
+            const variantsWithKanji = word.variants
+                .filter(validVariant.bind(null, kanji));
+
+            const validPriorityVariant = variantsWithKanji
+                .find(priorityVariant);
+
+            return {
+                variant: (
+                    validPriorityVariant ?
+                    validPriorityVariant :
+                    variantsWithKanji[0]
+                ),
+                meanings: word.meanings,
+            };
+        });
+    return converted;
+}
+
+module.exports = {
+    grade,
+    jlpt,
+    unicode,
+    prioritiseWords,
+    wordsForKanji,
+};
---
+++ tests/kanji_test.js
@@ -0,0 +1,149 @@
+const o = require('ospec');
+const Kanji = require('../src/kanji');
+const util = require('util');
+
+o.spec('prioritiseWords', () => {
+    o('words where a variant with the kanji is a proprity sort higher', () => {
+        const priorityValid = {
+            'variants': [
+                { 'written': 'お国', 'priorities': ['spec1'] },
+            ],
+        };
+
+        const nonPriorityValid = {
+            'variants': [
+                { 'written': 'お国', 'priorities': [] },
+            ],
+        };
+
+        const apiWords = [ nonPriorityValid, priorityValid ];
+
+        const words = Kanji.prioritiseWords('国', apiWords);
+
+        const expected = [ priorityValid, nonPriorityValid ];
+        o(words).deepEquals(expected);
+    });
+
+    o('the more priorities a variant with the kanji has the higher it sorts', () => {
+        const onePriorityValid = {
+            'variants': [
+                { 'written': 'お国', 'priorities': ['spec1'] },
+            ],
+        };
+
+        const twoPrioritiesValid = {
+            'variants': [
+                { 'written': 'お国', 'priorities': ['spec1', 'news1'] },
+            ],
+        };
+
+        const apiWords = [ onePriorityValid, twoPrioritiesValid ];
+
+        const words = Kanji.prioritiseWords('国', apiWords);
+
+        const expected = [ twoPrioritiesValid, onePriorityValid ];
+        o(words).deepEquals(expected);
+    })
+
+    o('words where the first variant has the kanji sort higher', () => {
+        const firstValid = {
+            'variants': [
+                { 'written': 'お国', 'priorities': [] },
+                { 'written': 'お', 'priorities': [] },
+            ],
+        };
+        
+        const firstNonValid = {
+            'variants': [
+                { 'written': 'お', 'priorities': [] },
+                { 'written': 'お国', 'priorities': [] },
+            ],
+        };
+
+        const apiWords = [ firstNonValid, firstValid ];
+
+        const words = Kanji.prioritiseWords('国', apiWords);
+
+        const expected = [ firstValid, firstNonValid ];
+        o(words).deepEquals(expected);
+    });
+
+    o('words with any priority variant sort higher', () => {
+        const priorityNonValid = {
+            'variants': [
+                { 'written': 'お', 'priorities': ['spec1'] },
+                { 'written': 'お国', 'priorities': [] },
+            ],
+        };
+
+        const noPriority = {
+            'variants': [
+                { 'written': 'お', 'priorities': [] },
+                { 'written': 'お国', 'priorities': [] },
+            ],
+        };
+
+        const apiWords = [ noPriority, priorityNonValid ];
+
+        const words = Kanji.prioritiseWords('国', apiWords);
+
+        const expected = [ priorityNonValid, noPriority ];
+        o(words).deepEquals(expected);
+    });
+
+    o('shorter words sort higher', () => {
+        const shorter = {
+            'variants': [
+                { 'written': 'お国', 'priorities': [] },
+            ],
+        };
+
+        const longer = {
+            'variants': [
+                { 'written': 'お国お', 'priorities': [] },
+            ],
+        };
+
+        const apiWords = [ longer, shorter ];
+
+        const words = Kanji.prioritiseWords('国', apiWords);
+
+        const expected = [ shorter, longer ];
+        o(words).deepEquals(expected);
+    });
+});
+
+o.spec('presentWords', () => {
+
+
+    // presents all three variants when search for 'stone', but not when search
+    // for other kanji in third
+    const word = {
+        "variants": [
+            {
+                "written": "かんらん石",
+                "pronounced": "かんらんせき",
+                "priorities": []
+            },
+            {
+                "written": "カンラン石",
+                "pronounced": "カンランせき",
+                "priorities": []
+            },
+            {
+                "written": "橄欖石",
+                "pronounced": "かんらんせき",
+                "priorities": []
+            }
+        ],
+        "meanings": [
+            {
+                "glosses": [
+                    "olivine",
+                    "peridot"
+                ]
+            }
+        ]
+    };
+
+});
